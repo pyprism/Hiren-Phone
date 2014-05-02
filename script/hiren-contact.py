b7148d1bcad46127532b579e971fcf9acfd2fdb0
pyprism <git.pyprism@gmail.com>
2014-05-02 16:23:05 +0600

    Contact script completed

--- script/hiren-contact.py
+++ script/hiren-contact.py
@@ -1,15 +1,54 @@
+#Date : May 1 , 2014 . Hiren is Missing for 1 month
 #!/usr/bin/env python
 
-import pymongo , re ,os
+
+import pymongo , re ,os,shutil ,time
+
+def Database(name,number):
+    client = pymongo.MongoClient()
+    db = client['Hiren-Phone']
+    collection = db['Contacts']
+    duplicate = collection.find_one({'Number' : number})
+    if not duplicate:
+        data = { 'Name' : name ,
+             'Number' : number ,
+             'Another Name' : '' ,
+             'Time' : time.strftime('%d-%m-%Y' )}
+        collection.insert(data)
+        print str(name).upper() + ' Is Added'
+    elif duplicate['Name'] == name:
+        print str(name) + ' is already exist'
+    else:
+        collection.update({
+            "_id" : duplicate["_id"],
+            'Another Name' : '' + ', ' + name
+        })
+
 
 def main():
     filelist = os.listdir('../sample/')
     for i in filelist:
+        counter = 0
         if i.endswith('.vcf'):
             fileContent = open('../sample/' + i )
-            fileContent.read()
+            for i in fileContent.readlines():
+                if counter == 2:
+                    name = re.search(r'[^N:][\d\w\s?]+' , i)
+                elif counter == 3:
+                    phone = re.search(r'[^VOICE:][\d\*?\\#?]+' , i)
+                else:pass
+                counter = counter + 1
+            if not os.path.exists('../sample/finalContact/'):
+                os.mkdir('../sample/finalContact/')
+                os.chdir("../sample/finalContact/")
+            else:
+                os.chdir("../sample/finalContact/")
+            shutil.move('../sample/' + i , '../sample/finalContact/' + i)
+            Database(name,phone)
+
 
 
 
 if __name__ == '__main__':
     main()
+
