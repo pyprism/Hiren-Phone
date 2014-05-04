e5e9adc9c1a8ba27e3bdbf5d27a230aebb7c8252
pyprism <git.pyprism@gmail.com>
2014-05-04 22:42:29 +0600

    Fixed N;ENCODING=QUOTED-PRINTABLE: issue

--- script/hiren-contact.py
+++ script/hiren-contact.py
@@ -3,29 +3,29 @@
 #!/usr/bin/env python
 
 
-import pymongo , re ,os,shutil ,time
+import pymongo, re, os, shutil, time
 
 client = pymongo.MongoClient()
 db = client['Hiren-Phone']
 collection = db['Contacts']
 
 
-def database(name,number):
+def database(name, number):
     duplicate = collection.find_one({'Number': number})
     if not duplicate:
-        data = { 'Name': name,
-            'Number': number,
-             'Another Name': '',
-             'Time': time.strftime('%d-%m-%Y')}
+        data = {'Name': name,
+                'Number': number,
+                'Another Name': '',
+                'Time': time.strftime('%d-%m-%Y')}
         collection.insert(data)
         print str(name).upper() + ' Is Added'
     elif duplicate['Name'] == name:
         print str(name) + ' is already exist'
     else:
-        collection.update({
-            "_id": duplicate["_id"],
-            'Another Name': duplicate['Another Name'] + '  ' + name
-        })
+        collection.update({"_id": duplicate["_id"]},
+                          {'Another Name': duplicate['Another Name'] + '  ' + name},
+                          upsert=False
+        )
         print str(name) + ' is updated'
 
 
@@ -33,20 +33,25 @@ def main():
     filelist = os.listdir('../sample/')
     for i in filelist:
         counter = 0
+        moveornot = ""
         if i.endswith('.vcf'):
             fileContent = open('../sample/' + i)
-            for i in fileContent.readlines():
+            for lines in fileContent.readlines():
                 if counter == 2:
-                    name = re.search(r'[^N:][\d\w\s]+', i)
+                    if lines.startswith('N;ENCODING'):
+                        print 'Special Case detected ' + i  # N;ENCODING=QUOTED-PRINTABLE:
+                        moveornot = True
+                    else:
+                        name = re.search(r'[^N:][\d\w\s]+', lines)
                 elif counter == 3:
-                    phone = re.search(r'[^VOICE:][\d\*\+\#]+', i)
-                else:pass
+                    phone = re.search(r'[^VOICE:][\d\*\+\#]+', lines)
                 counter = counter + 1
             fileContent.close()
             if not os.path.exists('../sample/finishedContact/'):
                 os.mkdir('../sample/finishedContact/')
-            shutil.move('../sample/' + i, '../sample/finishedContact/' + i)
-            database(name.group(), phone.group())
+            if moveornot:
+                shutil.move('../sample/' + i, '../sample/finishedContact/' + i)
+            database(name.group()[:-2], phone.group())
 
 
 if __name__ == '__main__':
