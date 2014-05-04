5e0a024b535e624179f221e8dbc7e5f11fba1a85
pyprism <git.pyprism@gmail.com>
2014-05-04 16:47:34 +0600

    Partially completed

--- script/hiren-message.py
+++ script/hiren-message.py
@@ -3,4 +3,22 @@ __author__ = 'prism'
 #Just for messages !
 #!/usr/bin/env python
 
-import pymongo , re ,os,shutil ,time
+import pymongo,re,os,shutil,time
+
+client = pymongo.MongoClient()
+db = client['Hiren-Phone']
+collection = db['Message']
+
+
+def database(number,filename,message,date,time,boxtype):
+    pass
+
+
+def main():
+    filelist = os.listdir('../sample/')
+    for i in filelist:
+        if i.endswith('.vmg'):
+            filecontent = open('../sample/' + i)
+            number = re.search(r'[TEL:+]\d+\n', filecontent.read().decode('utf-16'))
+
+            filecontent.close()
