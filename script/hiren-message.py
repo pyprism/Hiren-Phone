da7d5f09cecb73c29dabc372d04ef70ea10e7394
pyprism <git.pyprism@gmail.com>
2014-05-04 21:36:27 +0600

    Message script complete :D

--- script/hiren-message.py
+++ script/hiren-message.py
@@ -3,15 +3,26 @@ __author__ = 'prism'
 #Just for messages !
 #!/usr/bin/env python
 
-import pymongo,re,os,shutil,time
+import pymongo, re, os, shutil
 
 client = pymongo.MongoClient()
 db = client['Hiren-Phone']
 collection = db['Message']
 
 
-def database(number, filename, message, date, time, boxtype):
-    pass
+def database(number, filename, message, date, timer, boxtype):
+    duplicate = collection.find_one({'Number': number, 'Message': message, 'Date': date, 'Time': timer})
+    if not duplicate:
+        data = {'Number': number,
+                'Message': message,
+                'Date': date,
+                'Time': timer,
+                'Box Type': boxtype,
+                'File Name': filename}
+        collection.insert(data)
+        print 'Data Inserted ;) Hiren ! '
+    else:
+        print 'Duplicate message'
 
 
 def main():
@@ -21,7 +32,7 @@ def main():
             filecontent = open('../sample/' + i)
             number = re.search(r'[TEL:+]\d+\n', filecontent.read().decode('utf-16'))
             filecontent.seek(0, 0)  # Reset pointer
-            time = re.search(r'\d\d:\d\d:\d\d', filecontent.read().decode('utf-16'))
+            timer = re.search(r'\d\d:\d\d:\d\d', filecontent.read().decode('utf-16'))
             filecontent.seek(0, 0)
             date = re.search(r'[Date]:[\d\.]+', filecontent.read().decode('utf-16'))
             filecontent.seek(0, 0)
@@ -41,9 +52,9 @@ def main():
             #Move sorted files to different folder
             if not os.path.exists('../sample/finishedMessage/'):
                 os.mkdir('../sample/finishedMessage/')
-                os.chdir("../sample/finishedMessage/")
-            else:
-                os.chdir("../sample/finishedMessage/")
             shutil.move('../sample/' + i, '../sample/finishedMessage/' + i)
             filecontent.close()
-            database(number.group(), i, message, date.group().lstrip('e:'), time.group(), boxtype)
+            database(number.group()[:-1], i, message, date.group().lstrip('e:'), timer.group(), boxtype)
+
+if __name__ == '__main__':
+    main()
